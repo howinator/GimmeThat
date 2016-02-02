3f0e5e9443c622a1e118270b2f9f8ab1123dd57b
Howie Benefiel <hben592@gmail.com>
2016-02-01 22:29:24 -0600

    Implemented edit and redirect

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url(r'^$', views.post_list),
     url(r'^create/$', views.post_create),
     url(r'^(?P<id>\d+)/$', views.post_detail, name='detail'),
-    url(r'^update/$', views.post_update),
+    url(r'^(?P<id>\d+)/edit/$', views.post_update, name='update'),
     url(r'^delete/$', views.post_delete),
 ]
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,12 +1,22 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 
+from .forms import PostForm
 from .models import Post
 # Create your views here.
 
 
 def post_create(request):
-    return HttpResponse("<h1>Create</h1>")
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        return HttpResponseRedirect(instance.get_absolute_url())
+    context = {
+        "form": form,
+    }
+
+    return render(request, "post_form.html", context)
 
 
 def post_detail(request, id=None):
@@ -34,11 +44,21 @@ def post_list(request):
             "title": "List"
         }
     return render(request, "index.html", context)
-    # return HttpResponse("<h1>List</h1>")
 
 
-def post_update(request):
-    return HttpResponse("<h1>Update</h1>")
+def post_update(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    form = PostForm(request.POST or None, instance=instance)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        return HttpResponseRedirect(instance.get_absolute_url())
+    context = {
+        "title": instance.title,
+        "instance": instance,
+        "form": form,
+    }
+    return render(request, "post_form.html", context)
 
 
 def post_delete(request):
