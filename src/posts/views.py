4e39ed575ea2d48a526178d727e49235c9cf0250
Howie Benefiel <hben592@gmail.com>
2016-02-03 11:53:19 -0600

    implemented messages and delete FBV

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,5 +1,6 @@
+from django.contrib import messages
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .forms import PostForm
 from .models import Post
@@ -11,7 +12,11 @@ def post_create(request):
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
+        # messages success
+        messages.success(request, "Succesfully created")
         return HttpResponseRedirect(instance.get_absolute_url())
+    else:
+        messages.error(request, "Not Succesfully Created")
     context = {
         "form": form,
     }
@@ -52,6 +57,8 @@ def post_update(request, id=None):
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
+        messages.success(
+            request, "<a href='#'>Item</a> Saved.", extra_tags='html_safe')
         return HttpResponseRedirect(instance.get_absolute_url())
     context = {
         "title": instance.title,
@@ -61,5 +68,8 @@ def post_update(request, id=None):
     return render(request, "post_form.html", context)
 
 
-def post_delete(request):
-    return HttpResponse("<h1>Delete</h1>")
+def post_delete(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    instance.delete()
+    messages.success(request, "Succesfully deleted")
+    return redirect("posts:list")
