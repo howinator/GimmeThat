a00960f8d3cd03f1ad2a034e0c8623a9f723ddbc
Howie Benefiel <hben592@gmail.com>
2016-02-05 20:58:03 -0600

    Started implemeting pagination. For some reason, it's not pasting the info for the next page at the bottom. Template problem???

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .forms import PostForm
 from .models import Post
@@ -34,18 +35,22 @@ def post_detail(request, id=None):
 
 def post_list(request):
 
-    object_list = Post.objects.all()
-    if request.user.is_authenticated():
+    queryset_list = Post.objects.all()
+    paginator = Paginator(queryset_list, 5)  # Show 25 object_list per page
 
-        context = {
-            "object_list": object_list,
-            "title": "My Post List"
-        }
-    else:
-        context = {
-            "object_list": object_list,
-            "title": "List"
-        }
+    page = request.GET.get('page')
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        queryset = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        queryset = paginator.page(paginator.num_pages)
+    context = {
+        "object_list": queryset,
+        "title": "My Post List"
+    }
     return render(request, "post_list.html", context)
 
 
