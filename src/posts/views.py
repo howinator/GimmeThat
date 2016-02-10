e68f4752aac00f0852aedef1ada8a536f8f1dfb1
Howie Benefiel <hben592@gmail.com>
2016-02-09 23:06:08 -0600

    Implemented facebook comments (halfway) and rolled out settings configuration tool

--- src/posts/views.py
+++ src/posts/views.py
@@ -16,6 +16,7 @@ def post_create(request):
     form = PostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         instance = form.save(commit=False)
+        instance.user = request.user
         instance.save()
         # messages success
         messages.success(request, "Succesfully created")
