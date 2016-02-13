b664082d87ba2d9b64c4be5642a9b35659209ac3
Howie Benefiel <hben592@gmail.com>
2016-02-13 01:04:57 -0600

    Almost finished setting up settings module. Still have to modify allowed hosts for production

--- src/GimmeThatDjango/settings/production.py
+++ src/GimmeThatDjango/settings/production.py
@@ -2,14 +2,14 @@ from GimmeThatDjango.settings.base import *
 
 DEBUG = False
 
-DB_PASSWORD = base.get_env_variable(DB_PASSWORD)
-DB_USER = base.get_env_variable(DB_USER)
+DB_PASSWORD = get_env_variable("DB_PASSWORD")
+DB_USER = get_env_variable("DB_USER")
 
 DATABASES = {
     "default": {
         "NAME": "gimmethatdb",
         "HOST": "localhost",
-        "USER": "gimmethatdbuser",
+        "USER": DB_USER,
         "PASSWORD": DB_PASSWORD,
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "PORT": ""
