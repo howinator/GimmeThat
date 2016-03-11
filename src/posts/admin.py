6400ddd7b8e044bd1622dd32579dcfe47ea5c4a2
Howie Benefiel <hben592@gmail.com>
2016-03-11 00:42:49 -0600

    Fixed spelling errors.

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post
+from .models import Post, Category
+
 
 class PostModelAdmin(admin.ModelAdmin):
-	list_display =["__str__", "date_added"]
-	list_display_links = ["date_added", "__str__"]
-	list_filter = ["date_added", "last_modified"]
-	search_fields = ["title", "content"]
-	class Meta:
-		model = Post
+    list_display = ["__str__", "date_added"]
+    list_display_links = ["date_added", "__str__"]
+    list_filter = ["date_added", "last_modified"]
+    search_fields = ["title", "content"]
+
+    class Meta:
+        model = Post
 
 admin.site.register(Post, PostModelAdmin)
+admin.site.register(Category)
