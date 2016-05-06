060708945785b201c504e5f75f67da705961cc7b
Howie Benefiel <hben592@gmail.com>
2016-05-05 21:09:19 -0400

    Fixed the email and the return.

--- src/contact/views.py
+++ src/contact/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.core.mail import send_mail
 from django.contrib import messages
 from django.shortcuts import redirect
+from django.conf import settings
 
 from .forms import ContactForm
 # Create your views here.
@@ -19,13 +20,17 @@ def contact_us(request):
         to_email = [to_email]
         formatted_message = (
             'You\'ve received mail from {person!s}.'
-            ' Their message is displayed below \n\n{message!s}').format(
-                person=instance.from_name, message=instance.message)
+            '\n\nTheir e-mail is {email!s}'
+            ' \n\nTheir message is displayed below \n\n{message!s}').format(
+                person=instance.from_name,
+                email=instance.from_email,
+                message=instance.message)
         send_mail('You got gimmeth.at mail!', formatted_message,
-                  instance.from_email, to_email)
+                  settings.EMAIL_HOST_USER, to_email)
         instance.save()
         messages.success(request, "Message sent! Thank you.")
-        redirect("posts:list")
+        print("about to redirect")
+        return redirect("posts:list")
     context = {
         "contact_form": form
     }
